74dee55e40601e63d00e0b3e47073c1bbb3322e3
Thor Venø Bundesen <thor3819@elev.tec.dk>
2020-03-16 15:04:36 +0100

    16/03 15:04- lavet indtilvidere

--- ExamenProjekt/ExamenProjekt/Program.cs
+++ ExamenProjekt/ExamenProjekt/Program.cs
@@ -34,10 +34,7 @@ namespace ExamenProjekt
                             Console.WriteLine(AlkaneName(ChainCount(sideChainTable[item].ToString())));
                         }
                     }
-                    else
-                    {
-                        Console.WriteLine("Ugyldigt input");
-                    }
+                    
                 }
                 else
                 {
@@ -49,20 +46,25 @@ namespace ExamenProjekt
         {
             Hashtable sideChainTable = new Hashtable();
             int chainPos = 0;
+            string sideChain="";
 
             for (int i = 0; i < input.Length; i++)
             {
                 if(input[i] == 'c'||input[i] == 'C')
                 {
-                    chainPos++;
+                    chainPos++;//Counts the number of c-atoms up to the start of the side chain
                 }
                 else if (input[i] == '(')
                 {
                     int sideStart = i;
-                    int sideEnd = input.IndexOf(')', sideStart);
+                    int sideEnd = input.IndexOf(')', sideStart);//creates a zero based index from the start parren to the end parren
 
-                    string sideChain = input.Substring(sideStart + 1, sideEnd - sideStart-1);
-
+                    if ( sideEnd - sideStart - 1>0)
+                    {
+                        sideChain = input.Substring(sideStart + 1, sideEnd - sideStart - 1);/*creates a string in the interval 
+                    between sideStart and sideEnd. Adding and subtracting 1 to account for the parentheses 
+                    */
+                    }
                     sideChainTable.Add(chainPos, sideChain);
                     i = sideEnd;
                 }
@@ -110,7 +112,11 @@ namespace ExamenProjekt
                     int sideStart = i;
                     int sideEnd = input.IndexOf(')', sideStart);
 
-                    input = input.Remove(sideStart, sideEnd - sideStart + 1);
+                    if (sideEnd - sideStart + 1>0)
+                    {
+                        input = input.Remove(sideStart, sideEnd - sideStart + 1);
+                    }
+                    
                 }
             }
             return input;
@@ -118,14 +124,14 @@ namespace ExamenProjekt
 
         private static bool Validate(string input)
         {
-            bool accept = false ;
-            int StartParren=0;
-            int EndParren=0;
-        
+            bool accept = false;
 
-            foreach (var item in input)
+            int StartParren = 0;
+            int EndParren = 0;
+
+            foreach (var item in input)//tjækker parrenteser
             {
-                if (item =='(')
+                if (item == '(')
                 {
                     StartParren++;
                 }
@@ -134,15 +140,53 @@ namespace ExamenProjekt
                 {
                     EndParren++;
                 }
+
             }
-            if (StartParren==EndParren)
+            if (StartParren == EndParren)
             {
                 accept = true;
             }
-           
+            else
+            {
+                Console.WriteLine("Ugyldigt indput: Du mangler pattenteser");
+            }
+
+            int StartparrenPos = 0;
+            int EndparrenPos = 0;
+            foreach (var item in input)//tjækker position af parrenteser
+            {
+                if (item != '(')
+                {
+                    StartparrenPos++;
+                }
+                if (item == '(')
+                {
+                    StartparrenPos++;
+                    break;
+                }
+            }
+            foreach (var item in input)
+            {
+                if (item != ')')
+                {
+                    EndparrenPos++;
+                }
+                if (item == ')')
+                {
+                    EndparrenPos++;
+                    break;
+                }
+            }
+            if (EndparrenPos < StartparrenPos)
+            {
+                accept = false;
+                Console.WriteLine("Ugyldigt indput: Dine parrenteser står forkert");
+            }
+            
 
             return accept;
         }
+
 
     }
 }
